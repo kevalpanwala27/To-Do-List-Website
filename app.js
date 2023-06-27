888d2c71304d54ea14d6c02b9ee99b867137d790
Keval Panwala <kevalpanwala08@gmail.com>
2023-06-27 13:01:41 +0530

    modify index.html and app.js

--- app.js
+++ app.js
@@ -5,10 +5,17 @@ const app = express();
 app.use(bodyParser.urlencoded({extended: true}));
 
 app.get("/", function (req, res) {
-    res.send("Hello")
+    var today = new Date();
+    var currentDay = today.getDay();
+    if (currentDay === 6 || currentDay === 0) {
+        res.write("<h1>Yay it's the weekend!</h1>");
+    } else {
+        res.sendFile(__dirname + "/index.html");
+    }
 });
 
 app.listen(3000, function () {
     console.log("Server started on port 3000");
 });
+
 
